a09690e217c69665e0624c3f0223151594cf1da3
putradwi <sampurnaputradwi140@gmail.com>
2023-05-29 21:41:51 +0700

    add reguler fontWeight

--- lib/app/styles/app_styles.dart
+++ lib/app/styles/app_styles.dart
@@ -19,4 +19,5 @@ class AppStyles {
 
   static const FontWeight semiBold = FontWeight.w600;
   static const FontWeight medium = FontWeight.w500;
+  static const FontWeight reguler = FontWeight.w400;
 }
