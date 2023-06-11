57641cbe86d9b33daefa83ba9b3afe5ae73fab66
Muhammad Ridhoi <muhammad.ridhoi@borwita.co.id>
2023-06-11 17:26:54 +0700

    Revert "Perubahan dari Ridhoi - versi 1"
    This reverts commit 4a885eff

--- lib/app/app_constants.dart
+++
@@ -1,5 +0,0 @@
-class AppConstants {
-  static const String assets = "assets/images/";
-  static const String camera1 = "${AppConstants.assets}camera_1.jpg";
-  static const String camera2 = "${AppConstants.assets}camera_2.jpg";
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:pa_rentalcam/booking/booking_screen.dart';
-import 'package:pa_rentalcam/detail_camera/detail_camera_screen.dart';
 import 'package:pa_rentalcam/screens/auth/login_screen.dart';
-import 'package:pa_rentalcam/screens/auth/register_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BookingPage(),
+      home: LoginPage(),
     );
   }
 }
